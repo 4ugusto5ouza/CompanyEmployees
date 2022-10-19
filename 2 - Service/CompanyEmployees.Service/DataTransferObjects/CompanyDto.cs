cce11ca82781549e0d6fde67397197ffb41560a0
Samuel Souza <augusto_souza@edu.unifil.br>
2022-10-18 22:07:19 -0300

    :sparkles:feat: implementado accept negotiation text/xml

--- 2 - Service/CompanyEmployees.Service/DataTransferObjects/CompanyDto.cs
+++ 2 - Service/CompanyEmployees.Service/DataTransferObjects/CompanyDto.cs
@@ -2,5 +2,9 @@
 
 namespace CompanyEmployees.Service.DataTransferObjects
 {
-    public  record CompanyDto(Guid Id, string Name, string FullAddress);
+    public record CompanyDto { 
+        public Guid Id { get; init; }
+        public string? Name { get; init; }
+        public string? FullAddress { get; init; }
+    }
 }
--- 2 - Service/CompanyEmployees.Service/DataTransferObjects/EmployeeDto.cs
+++ 2 - Service/CompanyEmployees.Service/DataTransferObjects/EmployeeDto.cs
@@ -2,5 +2,10 @@
 
 namespace CompanyEmployees.Service.DataTransferObjects
 {
-    public record class EmployeeDto(Guid Id, string Name, int Age, string Position);
+    public record class EmployeeDto { 
+        public Guid Id { get; init; }
+        public string? Name { get; init; }
+        public int Age { get; init; }
+        public string? Position { get; init; }
+    }
 }

df7b673b5ad3423bf523c332fe73f96e07a8e1d0
Samuel Souza <augusto_souza@edu.unifil.br>
2022-10-21 21:01:14 -0300

    :sparkles:feat: implementado validation atributes em employeeforcreationdto

--- 2 - Service/CompanyEmployees.Service/DataTransferObjects/Employees/EmployeeForCreactionDto.cs
+++ 2 - Service/CompanyEmployees.Service/DataTransferObjects/Employees/EmployeeForCreactionDto.cs
@@ -1,4 +1,18 @@
-﻿namespace CompanyEmployees.Service.DataTransferObjects.Employees
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompanyEmployees.Service.DataTransferObjects.Employees
 {
-    public record EmployeeForCreationDto(string Name, int Age, string Position);
+    public record EmployeeForCreationDto//(string Name, int Age, string Position);
+    {
+        [Required(ErrorMessage = "Employee name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the name field is 30 characters.")]
+        public string? Name { get; init; }
+
+        [Required(ErrorMessage = "Age is a required field.")]
+        public int Age { get; init; }
+
+        [Required(ErrorMessage = "Position is a required field.")]
+        [MaxLength(20, ErrorMessage = "Maximum length for the position field is 20 characters.")]
+        public string? Position { get; init; }
+    }
 }

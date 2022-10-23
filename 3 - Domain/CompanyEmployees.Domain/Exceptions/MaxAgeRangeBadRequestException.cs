ed4281fad37e5ed79615fba44afa75aeaee8ec28
Samuel Souza <augusto_souza@edu.unifil.br>
2022-10-23 19:29:35 -0300

    :sparkles:feat: implementado filtro minage and maxage in getemployees

---
+++ 3 - Domain/CompanyEmployees.Domain/Exceptions/MaxAgeRangeBadRequestException.cs
@@ -0,0 +1,11 @@
+﻿using CompanyEmployees.Domain.Exceptions.ExceptionsBase;
+
+namespace CompanyEmployees.Domain.Exceptions
+{
+    public class MaxAgeRangeBadRequestException : BadRequestException
+    {
+        public MaxAgeRangeBadRequestException() : base("Mas age can`t be less than min age.")
+        {
+        }
+    }
+}
--- 4 - Infrastructure/CompanyEmployees.Infrastructure/Repositories/EmployeeRepository.cs
+++ 4 - Infrastructure/CompanyEmployees.Infrastructure/Repositories/EmployeeRepository.cs
@@ -18,15 +18,23 @@ namespace CompanyEmployees.Infrastructure.Repositories
         {
         }
 
+        private IQueryable<Employee> GetEmployeesByCompanyConditionAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
+        {
+            var queryableEmployeesByCondition = FindByCondition(e => e.CompanyId.Equals(companyId)
+                                    && (e.Age >= employeeParameters.MinAge && e.Age <= employeeParameters.MaxAge), trackChanges);
+
+            return queryableEmployeesByCondition;
+        }
+
         public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
         {
-            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+            var employees = await GetEmployeesByCompanyConditionAsync(companyId, employeeParameters, trackChanges)
                                     .OrderBy(e => e.Name)
                                     .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
                                     .Take(employeeParameters.PageSize)
                                     .ToListAsync();
 
-            var count = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges).CountAsync();
+            var count = await GetEmployeesByCompanyConditionAsync(companyId, employeeParameters, trackChanges).CountAsync();
 
             return new PagedList<Employee>(employees, count, employeeParameters.PageNumber, employeeParameters.PageSize);
         }

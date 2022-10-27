24f48e746c4a79418166adedf4c03381ff18afa9
Samuel Souza <augusto_souza@edu.unifil.br>
2022-10-26 21:44:28 -0300

    :sparkles:feat: criado controller para autenticação

---
+++ 1 - Application/CompanyEmployees.Application/Controllers/AuthenticationController.cs
@@ -0,0 +1,17 @@
+﻿using CompanyEmployees.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Application.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+
+        public AuthenticationController(IServiceManager service)
+        {
+            _service = service;
+        }
+    }
+}

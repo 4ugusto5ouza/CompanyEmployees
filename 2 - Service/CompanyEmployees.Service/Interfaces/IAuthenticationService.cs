8b7ee059aa9988d82c29d5b93591fb8d0c64be87
Samuel Souza <augusto_souza@edu.unifil.br>
2022-10-26 21:57:16 -0300

    :sparkles:feat: criado authentication service

---
+++ 2 - Service/CompanyEmployees.Service/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using CompanyEmployees.Service.DataTransferObjects.Users;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Service.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration);
+    }
+}
---
+++ 2 - Service/CompanyEmployees.Service/Services/AuthenticationService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using CompanyEmployees.Domain.Entities;
+using CompanyEmployees.Domain.Interfaces;
+using CompanyEmployees.Service.DataTransferObjects.Users;
+using CompanyEmployees.Service.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Service.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationService(ILoggerManager logger, IMapper mapper, UserManager<User> userManager, IConfiguration configuration)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        public Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
